a382d1931d2c0de277c695c6dae8f592b9a83d5a
cristiann <cristianandres1496@gmail.com>
2026-09-25 16:07:21 -0500

    db config

---
+++ services/src/database/schema.ts
@@ -0,0 +1,27 @@
+import { pgTable, uuid, text, pgEnum, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const accountRole = pgEnum("account_role", ["FREELANCER", "CLIENT"])
+
+export const accounts = pgTable( "accounts", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  auth_id: text("auth_id").notNull(),
+  email: text("email").notNull(),
+  role: accountRole().default("CLIENT").notNull(),
+  identityVerified: boolean("identifyVerified").default(false),
+  paymentMethodVerified: boolean("paymentMethodVerified").default(false),
+  isOnboardingComplete: boolean("isOnboardingComplete").default(false),
+  created_at: timestamp("created_at", { withTimezone: true }),
+  updated_at: timestamp("updated_at", { withTimezone: true }),
+})
+
+export const client_metadata = pgTable("client_metadata", {
+  id: uuid("id").defaultRandom().primaryKey(),
+  auth_id: text("auth_id").notNull().unique(),
+  role: text("role").notNull(),
+  company_name: text("company_name").notNull(),
+  company_size: text("company_size").notNull(),
+  industry: text("industry").notNull(),
+  company_description: text("company_description").notNull(),
+  created_at: timestamp("created_at", { withTimezone: true }),
+  updated_at: timestamp("updated_at", { withTimezone: true })
+})
